8d67724ef7940fa5b24f736477b6b9105e585176
ZFD <zfdandapple@gmail.com>
2018-11-12 13:12:06 +0800

    初始化提交

---
+++ tree/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  tree
+//
+//  Created by daole on 2018/11/12.
+//  Copyright © 2018 daole. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
